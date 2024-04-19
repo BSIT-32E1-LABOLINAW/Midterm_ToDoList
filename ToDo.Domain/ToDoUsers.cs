245f532125429043c8ab45f51e9fab1381a3b618
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-04-20 02:52:48 +0800

    Added Some Features & working To Do List with a revised User Interface.

--- ToDo.Domain/ToDoUsers.cs
+++ ToDo.Domain/ToDoUsers.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
-
 namespace ToDo.Domain
 {
     public class ToDo
     {
+        public int ItemNumber { get; set; }
+
         public int Id { get; set; }
 
-        public string Description { get; set; }
+        public string Task { get; set; }
 
-        public string Category { get; set; }
+        public string Date { get; set; }
 
         public bool Done { get; set; } = false;
     }
